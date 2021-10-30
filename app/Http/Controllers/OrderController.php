d75c9b14e133e3178bc52c0e967b7f47bfbd6208
Phuc <sucanabo@gmail.com>
2021-10-31 00:15:36 +0700

    done test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,78 +15,77 @@ use DB;
 class OrderController extends Controller
 {
     function create(Request $request){
-        return $request;
-        // DB::beginTransaction();
-        // try{
-        //     //validate
-        //     $attrs = $request->validate([
-        //         'address_id' => 'required',
-        //     ]);
-        //     //get item list
-        //     $items = $request['items'];
+        DB::beginTransaction();
+        try{
+            //validate
+            $attrs = $request->validate([
+                'address_id' => 'required',
+            ]);
+            //get item list
+            $items = $request['items'];
 
-        //     //create order
-        //     $order = Order::create(
-        //         [
-        //             'user_id' => auth()->user()->id,
-        //             'address_id' => $request['address_id'],
-        //             'shipping' => $request['shipping'],
-        //             'subtotal' => $request['subtotal'],
-        //             'voucher_discount' => $request['voucher_discount'],
-        //             'shipping_discount' => $request['shipping_discount'],
-        //             'promo' => $request['promo'],
-        //             'grandtotal' => $request['grandtotal'],
-        //             'content' => $request['content'],
-        //         ]
-        //     );
-        //     //create order items
+            //create order
+            $order = Order::create(
+                [
+                    'user_id' => auth()->user()->id,
+                    'address_id' => $request['address_id'],
+                    'shipping' => $request['shipping'],
+                    'subtotal' => $request['subtotal'],
+                    'voucher_discount' => $request['voucher_discount'],
+                    'shipping_discount' => $request['shipping_discount'],
+                    'promo' => $request['promo'],
+                    'grandtotal' => $request['grandtotal'],
+                    'content' => $request['content'],
+                ]
+            );
+            //create order items
             
-        //     foreach($items as $index){
-        //         $new_item =  OrderItem::create([
-        //             'order_id' => $order->id,
-        //             'product_id' => $index['product_id'],
-        //             'item_detail' => json_encode($index['item_detail']),
-        //             'quantity' => $index['quantity'],
-        //             'content' => $index['content'],
-        //          ]);
-        //      }
-        //     //create and updat transaction 
-        //     $latestOrder = Order::orderBy('created_at','DESC')->first();
-        //      $transaction = Transaction::create([
-        //         'user_id' =>  auth()->user()->id,
-        //         'delivery_method' => $request['delivery_method'],
-        //         'order_id' => $order->id,
-        //         'code' => '#'.str_pad($latestOrder->id + 1, 8, "0", STR_PAD_LEFT),
-        //         'type' => 'cash',
-        //         'mode' => 'online',
-        //         'status' => 'pending',
-        //      ]);
+            foreach($items as $index){
+                $new_item =  OrderItem::create([
+                    'order_id' => $order->id,
+                    'product_id' => $index['product_id'],
+                    'item_detail' => json_encode($index['item_detail']),
+                    'quantity' => $index['quantity'],
+                    'content' => $index['content'],
+                 ]);
+             }
+            //create and updat transaction 
+            $latestOrder = Order::orderBy('created_at','DESC')->first();
+             $transaction = Transaction::create([
+                'user_id' =>  auth()->user()->id,
+                'delivery_method' => $request['delivery_method'],
+                'order_id' => $order->id,
+                'code' => '#'.str_pad($latestOrder->id + 1, 8, "0", STR_PAD_LEFT),
+                'type' => 'cash',
+                'mode' => 'online',
+                'status' => 'pending',
+             ]);
 
-        //     // //delete user voucher
-        //     // $promos = $request['promo'];
-        //     // if($promos != null){
-        //     //     foreach($promos as $index){
-        //     //         UserVoucher::where('id',$index)->update(['status' => 0]);
-        //     //     }
-        //     // }
-        //     // //update user's point and level
-        //     // $new_point = $user->total_point + round($request['grandtotal']);
-        //     // User::where('id',$request->user_id)->update(['total_point' => $new_point]);
-        //     // $new_level = 0;
-        //     // $level_list = array([100,300,600,1000,3000,5000,10000]);//new, bzone, sivil, gold, platinum, diamond, V.I.P
-        //     // foreach($level_list as $key => $value){
-        //     //     if($new_point <= $value){
-        //     //         $new_level = $key;
-        //     //         break;
-        //     //     } 
-        //     // }
+            // //delete user voucher
+            // $promos = $request['promo'];
+            // if($promos != null){
+            //     foreach($promos as $index){
+            //         UserVoucher::where('id',$index)->update(['status' => 0]);
+            //     }
+            // }
+            // //update user's point and level
+            // $new_point = $user->total_point + round($request['grandtotal']);
+            // User::where('id',$request->user_id)->update(['total_point' => $new_point]);
+            // $new_level = 0;
+            // $level_list = array([100,300,600,1000,3000,5000,10000]);//new, bzone, sivil, gold, platinum, diamond, V.I.P
+            // foreach($level_list as $key => $value){
+            //     if($new_point <= $value){
+            //         $new_level = $key;
+            //         break;
+            //     } 
+            // }
 
-        //     DB::commit();
-        //     return response(['message' => 'success','data' => $transaction],200);
-        // }
-        // catch(Exception $e){
-        //     DB::rollback();
-        //     throw new Exception($e -> getMessage());
-        // }
+            DB::commit();
+            return response(['message' => 'success','data' => $transaction],200);
+        }
+        catch(Exception $e){
+            DB::rollback();
+            throw new Exception($e -> getMessage());
+        }
     }
 }
